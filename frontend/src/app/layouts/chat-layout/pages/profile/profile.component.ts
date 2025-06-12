f087e35e218878426870fbe45c49b12f8bfb9cb9
Kuby11 <saab112468@gmail.com>
2025-06-13 00:17:54 +0400

    created profile page

--- frontend/src/app/layouts/chat-layout/pages/profile/profile.component.ts
+++ frontend/src/app/layouts/chat-layout/pages/profile/profile.component.ts
@@ -1,29 +1,43 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit,} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal,} from '@angular/core';
 import { currentPageService } from '../../services';
-import { AuthService } from '../../../../auth/services/auth.service';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { ActivatedRoute } from '@angular/router';
+import { Profile, User } from '../../../../shared/interfaces';
+import { map } from 'rxjs';
+import { HlmAvatarComponent, HlmAvatarFallbackDirective, HlmAvatarImageDirective } from '@spartan-ng/ui-avatar-helm';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [HlmButtonDirective],
+  imports: [
+    HlmButtonDirective,
+    HlmAvatarComponent,
+    HlmAvatarFallbackDirective,
+    HlmAvatarImageDirective,
+    DatePipe
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
   providers: [],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent implements OnInit {
-  private readonly auth = inject(AuthService)
-  currentPageService = inject(currentPageService);
+  private readonly currentPageService = inject(currentPageService);
+  private readonly activatedRoute = inject(ActivatedRoute);
 
-  logout(){
-    this.auth.logout();
-  }
-
-  test(){
-    this.auth.getMe().subscribe(console.log);
-  }
+  profileData = signal<Profile | null>(null)
+  userData = signal<User | null>(null)
+  
+  data = this.activatedRoute.data
 
   ngOnInit() {
-    this.currentPageService.setPage('profile');    
+    this.data.pipe(
+      map((data)=>{
+        this.profileData.set(data['profile'])
+        this.userData.set(data['user'])
+      })
+    ).subscribe()
+    
+    this.currentPageService.setPage(this.userData()?.username + '`s profile');
   }
 }
