5fa207cbebd3733f38c4f002c75236054c6fabd9
Kuby11 <saab112468@gmail.com>
2025-06-13 23:03:35 +0400

    added profile edit dialog

--- frontend/src/app/layouts/chat-layout/pages/profile/profile.component.ts
+++ frontend/src/app/layouts/chat-layout/pages/profile/profile.component.ts
@@ -2,9 +2,22 @@ import { ChangeDetectionStrategy, Component, inject, OnInit, signal,} from '@ang
 import { currentPageService } from '../../services';
 import { ActivatedRoute } from '@angular/router';
 import { Profile, User } from '../../../../shared/interfaces';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 import { HlmAvatarComponent, HlmAvatarFallbackDirective, HlmAvatarImageDirective } from '@spartan-ng/ui-avatar-helm';
 import { DatePipe } from '@angular/common';
+import { UserService } from '../../../../shared/services/user.service';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { BrnDialogContentDirective, BrnDialogTriggerDirective } from '@spartan-ng/brain/dialog';
+import {
+  HlmDialogComponent,
+  HlmDialogContentComponent,
+  HlmDialogDescriptionDirective,
+  HlmDialogFooterComponent,
+  HlmDialogHeaderComponent,
+  HlmDialogTitleDirective,
+} from '@spartan-ng/ui-dialog-helm';
+
 
 @Component({
   selector: 'app-profile',
@@ -12,7 +25,17 @@ import { DatePipe } from '@angular/common';
     HlmAvatarComponent,
     HlmAvatarFallbackDirective,
     HlmAvatarImageDirective,
-    DatePipe
+    HlmButtonDirective,
+    HlmInputDirective,
+    HlmDialogComponent,
+    HlmDialogContentComponent,
+    HlmDialogDescriptionDirective,
+    HlmDialogFooterComponent,
+    HlmDialogHeaderComponent,
+    HlmDialogTitleDirective,
+    BrnDialogContentDirective,
+    BrnDialogTriggerDirective,
+    DatePipe,
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
@@ -21,21 +44,37 @@ import { DatePipe } from '@angular/common';
 })
 export class ProfileComponent implements OnInit {
   private readonly currentPageService = inject(currentPageService);
+  private readonly userService = inject(UserService);
   private readonly activatedRoute = inject(ActivatedRoute);
 
-  profileData = signal<Profile | null>(null)
-  userData = signal<User | null>(null)
-  
-  data = this.activatedRoute.data
+  profileData = signal<Profile | null>(null);
+  userData = signal<User | null>(null);
+  isCurrentUser = signal<boolean>(false);
+
+  data = this.activatedRoute.data;
 
   ngOnInit() {
-    this.data.pipe(
-      map((data)=>{
-        this.profileData.set(data['profile'])
-        this.userData.set(data['user'])
-      })
-    ).subscribe()
-    
+    this.data
+      .pipe(
+        map((data) => {
+          this.profileData.set(data['profile']);
+          this.userData.set(data['user']);
+        })
+      )
+      .subscribe();
+
+    this.userService
+      .getMe()
+      .pipe(
+        tap((data) => {
+          if (data.id === this.userData()!.id) {
+            this.isCurrentUser.set(true);
+          }
+        })
+      )
+      .subscribe();
+   
+
     this.currentPageService.setPage(this.userData()?.username + '`s profile');
   }
 }
