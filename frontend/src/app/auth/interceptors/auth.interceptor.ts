f58ad3ceb40dc3c6064ee4271fbef52dad795d16
Kuby11 <saab112468@gmail.com>
2025-06-17 20:38:45 +0400

    renamed refresh function service argument

--- frontend/src/app/auth/interceptors/auth.interceptor.ts
+++ frontend/src/app/auth/interceptors/auth.interceptor.ts
@@ -2,14 +2,19 @@ import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { BehaviorSubject, catchError, filter, switchMap, tap, throwError } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 let isRefreshing$ = new BehaviorSubject<boolean>(false);
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService)
+  const cookie = inject(CookieService)
   const access_token = authService.access_token;
   
-  if(!access_token) return next(req);
+  if(!access_token){ 
+    console.log('no access_token');
+    return next(req)
+  }
 
   if(isRefreshing$.value){
     return refresh(authService, req, next);
@@ -28,38 +33,39 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
 
 const refresh = (
-  service: AuthService, 
-  req: HttpRequest<any>, 
+  AuthService: AuthService,
+  req: HttpRequest<any>,
   next: HttpHandlerFn
 ) => {
-  if(!isRefreshing$.value){
+  if (!isRefreshing$.value) {
     isRefreshing$.next(true);
-    return service.refreshToken()
-    .pipe(
+    return AuthService.refreshToken().pipe(
       switchMap((res) => {
-        return next(addToken(req, res.access_token))
-          .pipe(
-            tap(() => isRefreshing$.next(false))
-          )
-      }),
-    )
+        return next(addToken(req, res.access_token)).pipe(
+          tap(() => isRefreshing$.next(false))
+        );
+      })
+    );
   }
 
-  if(req.url.includes('refresh'))  return next(addToken(req, service.access_token!));
-  
+  if (req.url.includes('refresh'))
+    return next(addToken(req, AuthService.access_token!));
 
   return isRefreshing$.pipe(
-    filter(isRefreshing => !isRefreshing ),
+    filter((isRefreshing) => !isRefreshing),
     switchMap(() => {
-      return next(addToken(req, service.access_token!))
+      return next(addToken(req, AuthService.access_token!));
     })
-  )
-}
+  );
+};
 
-const addToken = (req: HttpRequest<any>, token: string) => {
+const addToken = (
+  req: HttpRequest<any>,
+  token: string,
+) => {
   return req.clone({
     setHeaders: {
       Authorization: token,
     },
-  })
-}
+  });
+};
