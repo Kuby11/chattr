ca95751a9d37c3f298ee3ab9b8d3bf557da80af6
Kuby11 <saab112468@gmail.com>
2025-06-18 03:42:18 +0400

    used pipes in template

--- frontend/src/app/layouts/chat-layout/pages/chat-page/chat-page.component.ts
+++ frontend/src/app/layouts/chat-layout/pages/chat-page/chat-page.component.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectionStrategy, Component, effect, inject, input, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { ChatWidgetComponent } from '../../../../widgets/chat-widget/chat-widget.component';
 import { currentPageService } from '../../services';
-import { UserService } from '../../../../shared/services/user.service';
-import { User } from '../../../../shared/interfaces';
+import { Profile, User } from '../../../../shared/interfaces';
 import { firstValueFrom } from 'rxjs';
 import { HlmAvatarImageDirective, HlmAvatarComponent, HlmAvatarFallbackDirective } from '@spartan-ng/ui-avatar-helm';
 import { RouterLink } from '@angular/router';
 import { HlmSkeletonComponent } from '@spartan-ng/ui-skeleton-helm';
+import { ProfileService } from '../../../../shared/services/profile.service';
+import { FirstLetterPipe } from '../../../../shared/pipes/first-letter.pipe';
+import { ShortenUsernamePipe } from '../../../../shared/pipes/shorten-username.pipe';
 
 
 @Component({
@@ -17,6 +19,8 @@ import { HlmSkeletonComponent } from '@spartan-ng/ui-skeleton-helm';
     HlmAvatarFallbackDirective,
     HlmAvatarImageDirective,
     HlmSkeletonComponent,
+    ShortenUsernamePipe,
+    FirstLetterPipe,
     RouterLink,
   ],
   templateUrl: './chat-page.component.html',
@@ -25,9 +29,9 @@ import { HlmSkeletonComponent } from '@spartan-ng/ui-skeleton-helm';
 })
 export class ChatPageComponent implements OnInit {
   private readonly pageService = inject(currentPageService);
-  private readonly userService = inject(UserService);
+  private readonly profileService = inject(ProfileService);
 
-  users = signal<User[]>([]);
+  profiles = signal<Profile[]>([]);
   skeletonItems = signal<number[]>(Array(20).fill(0))
   isLoading = signal<boolean>(true);
 
@@ -37,10 +41,10 @@ export class ChatPageComponent implements OnInit {
   }
 
   private async loadUsers() {
-    const response = await firstValueFrom(this.userService.getAllUsers());
+    const response = await firstValueFrom(this.profileService.getAllProfiles());
     if (response) {
       this.isLoading.set(false);
     }
-    this.users.set(response)
+    this.profiles.set(response)
   }
 }
