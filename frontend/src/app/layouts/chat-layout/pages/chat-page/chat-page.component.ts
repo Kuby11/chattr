980a5bca7977ffb8fd712f7a1540dae910311c29
Kuby11 <saab112468@gmail.com>
2025-06-18 02:38:20 +0400

    made skeleton placeholder for users list

--- frontend/src/app/layouts/chat-layout/pages/chat-page/chat-page.component.ts
+++ frontend/src/app/layouts/chat-layout/pages/chat-page/chat-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, input, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, input, OnInit, signal } from '@angular/core';
 import { ChatWidgetComponent } from '../../../../widgets/chat-widget/chat-widget.component';
 import { currentPageService } from '../../services';
 import { UserService } from '../../../../shared/services/user.service';
@@ -6,6 +6,7 @@ import { User } from '../../../../shared/interfaces';
 import { firstValueFrom } from 'rxjs';
 import { HlmAvatarImageDirective, HlmAvatarComponent, HlmAvatarFallbackDirective } from '@spartan-ng/ui-avatar-helm';
 import { RouterLink } from '@angular/router';
+import { HlmSkeletonComponent } from '@spartan-ng/ui-skeleton-helm';
 
 
 @Component({
@@ -15,8 +16,8 @@ import { RouterLink } from '@angular/router';
     HlmAvatarComponent,
     HlmAvatarFallbackDirective,
     HlmAvatarImageDirective,
-    RouterLink
-
+    HlmSkeletonComponent,
+    RouterLink,
   ],
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.css',
@@ -26,8 +27,9 @@ export class ChatPageComponent implements OnInit {
   private readonly pageService = inject(currentPageService);
   private readonly userService = inject(UserService);
 
-  usersRef = signal<User[]>([])
-  users = this.userService.getAllUsers()
+  users = signal<User[]>([]);
+  skeletonItems = signal<number[]>(Array(20).fill(0))
+  isLoading = signal<boolean>(true);
 
   ngOnInit() {
     this.loadUsers();
@@ -35,7 +37,10 @@ export class ChatPageComponent implements OnInit {
   }
 
   private async loadUsers() {
-    const response = await firstValueFrom(this.users);
-    this.usersRef.set(response);
+    const response = await firstValueFrom(this.userService.getAllUsers());
+    if (response) {
+      this.isLoading.set(false);
+    }
+    this.users.set(response)
   }
 }
