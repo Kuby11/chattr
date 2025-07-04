561bcad245d0e1f2118c126546799dfd8d8f401c
Kuby11 <saab112468@gmail.com>
2025-07-05 02:21:14 +0400

    configured lazy loading for components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
 import { ChatLayoutComponent } from './layouts/main-layout/chat-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
 import { NotFoundComponent } from './features/not-found-page/not-found.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import { ProfileComponent } from './pages/profile/profile.component';
 import { HomePageComponent } from './pages/home/home-page.component';
-import { ChatPageComponent } from './pages/chat-page/chat-page.component';
 import { userResolver } from './entities/user/user.resolver';
 import { profileResolver } from './entities/profile/profile.resolver';
 import { authGuard } from './features/auth/guards/auth.guard';
@@ -22,22 +17,27 @@ export const routes: Routes = [
       {
         path: '',
         component: HomePageComponent,
+        loadChildren: () => import('./pages/home/home-page.component').then((m)=> m.HomePageComponent)
       },
       {
         path: 'settings',
-        component: SettingsComponent,
+        loadChildren: () => import('./pages/settings/settings.component').then((m) => m.SettingsComponent), 
       },
       {
         path: 'profile/:id',
-        component: ProfileComponent,
+        loadChildren: () => import('./pages/profile/profile.component').then((m) => m.ProfileComponent),
         resolve: {
           profile: profileResolver,
-          user: userResolver
-        }
+          user: userResolver,
+        },
       },
       {
         path: 'chats',
-        component: ChatPageComponent,
+        loadChildren: () => import('./pages/chat-page/chat-page.component').then((m) => m.ChatPageComponent),
+      },
+      {
+        path: 'friend',
+        loadChildren: () => import('./pages/friend-page/friend-page.component').then((m) => m.FriendPageComponent),
       },
     ],
   },
@@ -48,16 +48,17 @@ export const routes: Routes = [
     children: [
       {
         path: '',
-        component: LoginComponent,
+        loadChildren: () => import('./pages/login/login.component').then((m) => m.LoginComponent)
       },
       {
         path: 'register',
-        component: RegisterComponent,
+        loadChildren: () => import('./pages/register/register.component').then((m) => m.RegisterComponent)
       },
     ],
   },
   {
     path: '**',
     component: NotFoundComponent,
+    loadChildren: () => import('./features/not-found-page/not-found.component').then((m) => m.NotFoundComponent)
   },
 ];
