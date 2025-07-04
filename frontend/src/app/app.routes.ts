88f068d3de01321e1488148235247aba89bf8dfa
Kuby11 <saab112468@gmail.com>
2025-07-05 03:00:01 +0400

    fixed error

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,64 +1,59 @@
 import { Routes } from '@angular/router';
-import { ChatLayoutComponent } from './layouts/main-layout/chat-layout.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-import { NotFoundComponent } from './features/not-found-page/not-found.component';
-import { HomePageComponent } from './pages/home/home-page.component';
 import { userResolver } from './entities/user/user.resolver';
 import { profileResolver } from './entities/profile/profile.resolver';
 import { authGuard } from './features/auth/guards/auth.guard';
 import { authPageGuard } from './features/auth/guards/auth-page.guard';
 
+
 export const routes: Routes = [
   {
     path: '',
-    component: ChatLayoutComponent,
+    loadComponent: () => import('./layouts/main-layout/chat-layout.component').then(m => m.ChatLayoutComponent),
     canActivate: [authGuard],
     children: [
       {
         path: '',
-        component: HomePageComponent,
-        loadChildren: () => import('./pages/home/home-page.component').then((m)=> m.HomePageComponent)
+        loadComponent: () => import('./pages/home/home-page.component').then(m => m.HomePageComponent),
       },
       {
         path: 'settings',
-        loadChildren: () => import('./pages/settings/settings.component').then((m) => m.SettingsComponent), 
+        loadComponent: () => import('./pages/settings/settings.component').then(m => m.SettingsComponent),
       },
       {
         path: 'profile/:id',
-        loadChildren: () => import('./pages/profile/profile.component').then((m) => m.ProfileComponent),
+        loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent),
         resolve: {
           profile: profileResolver,
-          user: userResolver,
-        },
+          user: userResolver
+        }
       },
       {
         path: 'chats',
-        loadChildren: () => import('./pages/chat-page/chat-page.component').then((m) => m.ChatPageComponent),
+        loadComponent: () => import('./pages/chat-page/chat-page.component').then(m => m.ChatPageComponent),
       },
       {
-        path: 'friend',
-        loadChildren: () => import('./pages/friend-page/friend-page.component').then((m) => m.FriendPageComponent),
-      },
+        path: "friends",
+        loadComponent: () => import('./pages/friend-page/friend-page.component').then(m => m.FriendPageComponent)
+      }
     ],
   },
   {
     path: 'auth',
-    component: AuthLayoutComponent,
+    loadComponent: () => import('./layouts/auth-layout/auth-layout.component').then(m => m.AuthLayoutComponent),
     canActivate: [authPageGuard],
     children: [
       {
         path: '',
-        loadChildren: () => import('./pages/login/login.component').then((m) => m.LoginComponent)
+        loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
       },
       {
         path: 'register',
-        loadChildren: () => import('./pages/register/register.component').then((m) => m.RegisterComponent)
+        loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent),
       },
     ],
   },
   {
     path: '**',
-    component: NotFoundComponent,
-    loadChildren: () => import('./features/not-found-page/not-found.component').then((m) => m.NotFoundComponent)
+    loadComponent: () => import('./features/not-found-page/not-found.component').then(m => m.NotFoundComponent),
   },
 ];
