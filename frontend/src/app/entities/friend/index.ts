9ea3065a0811dd702ca40b3782a54211f398245c
Kuby11 <saab112468@gmail.com>
2025-07-13 16:28:09 +0400

    added imports for pipes in friend entity index file

--- frontend/src/app/entities/friend/index.ts
+++ frontend/src/app/entities/friend/index.ts
@@ -1,3 +1,5 @@
 export * from './friend.service'
 export * from './friend.interface';
 export * from './friend.interface';
+export * from './pipes/filter-friend-list.pipe'
+export * from './pipes/sort-friend-list.pipe'
