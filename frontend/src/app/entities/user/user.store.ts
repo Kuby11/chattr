84ead7682591587ea103dd238f6017d6d4bf17ea
Kuby11 <saab112468@gmail.com>
2025-07-14 16:31:43 +0400

    corrected store name and added findMany method

---
+++ frontend/src/app/entities/user/user.store.ts
@@ -0,0 +1,38 @@
+import { signalStore, withState, withMethods, patchState } from '@ngrx/signals'
+import { User, UserService } from '.';
+import { inject } from '@angular/core';
+
+interface UserStore { 
+	currentUser: User | undefined,
+	users: User[] | undefined,
+}
+
+const initialValue: UserStore  = {
+  currentUser: undefined,
+	users: undefined
+};
+
+export const UserStore = signalStore(
+	{ providedIn: 'root' },
+	withState(initialValue),
+	withMethods(state => {
+		const api = inject(UserService)
+		return {			
+			loadCurrentUser: () => {
+				api.getMe()
+				.subscribe((currentUser: User) => {
+					patchState(state, { currentUser })
+				})
+			},
+			setCurrentUser: (currentUser: User ) => patchState(state, { currentUser }),
+			resetCurrentUser: () => patchState(state, initialValue),
+			findUsers: (searchQuery: string) => {
+				api.findUsersByQuery(searchQuery)
+				.subscribe((usersRes) => {
+					const users = usersRes
+					patchState(state, { users })
+				})
+			},
+		}
+	})
+)
