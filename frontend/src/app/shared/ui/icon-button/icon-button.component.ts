07495550bf48218c6b8a674bf1537654308b250b
Kuby11 <saab112468@gmail.com>
2025-07-18 19:39:22 +0400

    made disabled variant

--- frontend/src/app/shared/ui/icon-button/icon-button.component.ts
+++ frontend/src/app/shared/ui/icon-button/icon-button.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { BrnTooltipContentDirective } from '@spartan-ng/brain/tooltip';
 import { HlmTooltipComponent, HlmTooltipTriggerDirective } from '@spartan-ng/ui-tooltip-helm';
 
-type ButtonVariants = 'default' | 'destructive'
+type ButtonVariants = 'default' | 'destructive' | 'disabled'
 
 @Component({
 	selector: 'app-icon-button',
@@ -15,15 +15,20 @@ type ButtonVariants = 'default' | 'destructive'
 		<hlm-tooltip>
 			<button
 				hlmTooltipTrigger
+				[disabled]="isDisabled()"
 				[aria-describedby]="tooltipContent()"
 				class="flex sm:p-2 p-1 rounded-xl transition hover:bg-destructive/20"
-				[class]="variant() === 'destructive' 
-					? 'hover:bg-destructive/20 text-destructive' 
-					: 'hover:bg-primary/20 text-primary'"
+				[class]="
+					variant() ===  'destructive' ? 'hover:bg-destructive/20 text-destructive' : 
+					variant() === 'disabled' ? 'text-foreground/30 hover:bg-transparent' : 
+						'hover:bg-primary/20 text-primary'"
+				[class.cursor-pointer]="!isDisabled()" 
 			>
 				<ng-content select="ng-icon"/>
 			</button>
-			<span *brnTooltipContent [class]="variant() === 'destructive' ? 'text-destructive' : ''">{{ tooltipContent() }}</span>
+			@if(!isDisabled()){
+				<span *brnTooltipContent [class]="variant() === 'destructive' ? 'text-destructive' : ''">{{ tooltipContent() }}</span>
+			}
 		</hlm-tooltip>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush
@@ -32,4 +37,5 @@ type ButtonVariants = 'default' | 'destructive'
 export class IconButtonComponent {
 	tooltipContent = input.required<string>()
 	variant = input<ButtonVariants>('default')
+	isDisabled = computed(()=> this.variant() === 'disabled')
 }
