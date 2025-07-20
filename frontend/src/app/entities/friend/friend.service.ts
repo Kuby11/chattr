64d76e7720417359356a06ce2d250d73b97e4f8e
Kuby11 <saab112468@gmail.com>
2025-07-20 18:33:36 +0400

    made new methods in service and store

--- frontend/src/app/entities/friend/friend.service.ts
+++ frontend/src/app/entities/friend/friend.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Friend } from './friend.interface';
+import { Friend } from './interfaces/friend.interface';
 import { API_URL } from '@environment';
+import { FriendRequest } from './interfaces/friend-request.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +10,32 @@ import { API_URL } from '@environment';
 export class FriendService {
   private readonly http = inject(HttpClient);
 
-  getCurrentFriends() {
+  getFriends() {
     return this.http.get<Friend[]>(`${API_URL}/friend/friends`)
+  }
+
+  getFriendRequests(){
+    return this.http.get<FriendRequest[]>(`${API_URL}/friend/friend-requests`)
   }
 
   removeFriend(friendId: string) {
     return this.http.delete(`${API_URL}/friend/${friendId}`)
   }
+
+  sendFriendRequest(receiverId: string){
+    return this.http.post(`${API_URL}/friend/send-friend-request/${receiverId}`, {})
+  }
+
+  acceptFriendRequest(requestId: string){
+    return this.http.post<FriendRequest>(`${API_URL}/friend/accept-friend-request/${requestId}`, {})
+  }
+  
+  declineFriendRequest(requestId: string){
+    return this.http.post<FriendRequest>(`${API_URL}/friend/decline-friend-request/${requestId}`, {})
+  }
+  
+  cancelFriendRequest(requestId: string){
+    return this.http.delete<FriendRequest>(`${API_URL}/friend/friend-request/${requestId}`, {})
+  }
+
 }
--- frontend/src/app/entities/friend/friend.store.ts
+++ frontend/src/app/entities/friend/friend.store.ts
@@ -1,11 +1,18 @@
 import { signalStore, withState, withMethods, patchState, withComputed } from '@ngrx/signals'
-import { Friend } from './friend.interface';
+import { Friend } from './interfaces/friend.interface';
 import { inject } from '@angular/core';
 import { FriendService } from './friend.service';
-import { delay, tap, throttle } from 'rxjs';
+import { delay, filter, map, tap, throttle } from 'rxjs';
+import { FriendRequest } from './interfaces/friend-request.interface';
 
-const initialValue: { friends: Friend[] | undefined } = {
-  friends: undefined,
+const initialValue: { 
+	friends: Friend[], 
+	friendRequests: FriendRequest[],
+	isLoading: boolean
+} = {
+  friends: [],
+	friendRequests: [],
+	isLoading: false
 };
 
 export const friendsStore = signalStore(
@@ -17,14 +24,14 @@ export const friendsStore = signalStore(
 		return {
 			loadFriends: () => {
 				let friends: undefined | Friend[] = undefined
-				api.getCurrentFriends()
+				api.getFriends()
 				.subscribe((friendRes: Friend[])=>{
 					friends = friendRes
 					patchState(state, { friends })
 				})
 			},
 			removeFriend(friendId: string){
-				const friends = state.friends()?.filter(item => item.friendOf.id != friendId)
+				const friends = state.friends()?.filter(item => item.friendOf.id !== friendId)
 				api.removeFriend(friendId)
 				.pipe(
 					delay(50),
@@ -32,8 +39,41 @@ export const friendsStore = signalStore(
 						patchState(state, { friends })
 					})
 				)
-				.subscribe()
-				patchState(state, { friends })
+				.subscribe(()=> patchState(state, { friends }))
+			},
+
+			isFriend(id: string){
+				if(state.friends()?.length === 0) {
+					this.loadFriends()
+				}
+				return state.friends()?.find((friend) => friend.friendOf.id === id)
+			},
+
+			getFriendRequests(){
+				patchState(state, { isLoading: true })
+				api.getFriendRequests()
+				.pipe(
+					map(requests => requests.filter(request => request.status === 'PENDING'))
+				)
+				.subscribe(friendRequests => patchState(state, { friendRequests, isLoading: false }))
+			},
+
+			acceptFriendRequest(requestId: string){
+				const friendRequests = state.friendRequests().filter(request => request.id !== requestId) 
+				api.acceptFriendRequest(requestId)
+				.subscribe(()=> patchState(state, { friendRequests }))
+			},
+
+			declineFriendRequest(requestId: string){
+				const friendRequests = state.friendRequests().filter(request => request.id !== requestId) 
+				api.acceptFriendRequest(requestId)
+				.subscribe(()=> patchState(state, { friendRequests }))
+			},
+
+			cancelFriendRequest(requestId: string){
+				const friendRequests = state.friendRequests().filter(request => request.id !== requestId) 
+				api.cancelFriendRequest(requestId)
+				.subscribe(()=> patchState(state, { friendRequests }))
 			}
 		}
 	}),
