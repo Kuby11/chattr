6b1f67e8b8fc54b4a7f5575f5474af84571a6563
Kuby11 <saab112468@gmail.com>
2025-07-21 13:51:06 +0400

    added variant input to the component

--- frontend/src/app/shared/ui/avatar/avatar.component.ts
+++ frontend/src/app/shared/ui/avatar/avatar.component.ts
@@ -11,9 +11,9 @@ import { FirstLetterPipe } from '../../pipes/first-letter.pipe';
 		FirstLetterPipe,
 	],
 	template: `
-		<hlm-avatar>
+		<hlm-avatar [variant]="variant()">
 			<img [src]="avatarUrl()" alt="users avatar" hlmAvatarImage>
-			<span class="text-background bg-primary" hlmAvatarFallback>{{ fallback()! | firstLetter }}</span>
+			<span class="text-background bg-primary"  hlmAvatarFallback>{{ fallback()! | firstLetter }}</span>
 		</hlm-avatar>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,4 +22,5 @@ import { FirstLetterPipe } from '../../pipes/first-letter.pipe';
 export class AvatarComponent {
 	avatarUrl = input<string | undefined>('')
 	fallback = input<string | undefined>('')
+	variant = input<'large' | 'medium' | 'small'>('medium')
 }
