52ba4915cfb01a592b0cf0af6608f3b044e0f235
Kuby11 <saab112468@gmail.com>
2025-07-26 22:29:39 +0400

    added RoomModule to ChatModule providers for enhanced chat functionality

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessageModule } from './message/message.module';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RoomModule } from './room/room.module';
 
 @Module({
-	providers: [MessageModule, PrismaService],
-	exports: [MessageModule]
+	providers: [MessageModule, PrismaService, RoomModule],
+	exports: [MessageModule],
 })
 export class ChatModule {}
