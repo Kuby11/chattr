33107ea8b4ccdfbcb0a69dcf520013cf3df4070c
Kuby11 <saab112468@gmail.com>
2025-08-04 21:57:25 +0400

    refactored message input and made component out of it

---
+++ frontend/src/app/widgets/message-input/message-input.component.ts
@@ -0,0 +1,44 @@
+import { ChangeDetectionStrategy, Component, computed, ElementRef, inject, model, output, viewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MessageService } from '../../entities/message';
+import { roomStore } from '../../entities/room';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+
+@Component({
+  selector: 'app-message-input',
+  imports: [
+    FormsModule,
+    HlmInputDirective,
+    HlmButtonDirective
+  ],
+  templateUrl: './message-input.component.html',
+  styleUrl: './message-input.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MessageInputComponent {
+  private readonly messageService = inject(MessageService)
+  private readonly roomStore = inject(roomStore)
+  private readonly messageInput = viewChild<ElementRef<HTMLElement>>('el')
+
+  chatOffset = computed(() => {
+    if(!this.messageInput()?.nativeElement){
+      return 0
+    }else{
+      return (this.messageInput()!.nativeElement.offsetHeight / 16) + 1
+    }
+  })
+
+  currentRoomId = computed(() => this.roomStore.currentRoomId())
+
+  ngOnInit(){
+    console.log(this.chatOffset())
+  }
+
+  onSubmit(form: NgForm){
+    if(form.valid && form.value.messageContent){
+      this.messageService.sendMessage(form.value.messageContent, this.currentRoomId()!)
+    }
+  }
+
+}
