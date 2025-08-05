5bbbd42ea9cbc692154052803338468f89bb1a30
Kuby11 <saab112468@gmail.com>
2025-08-05 23:08:12 +0400

    refactored message bubble component to include user avatar and sender verification; updated template and TypeScript logic for improved display and functionality

--- frontend/src/app/widgets/message-bubble/message-bubble.component.ts
+++ frontend/src/app/widgets/message-bubble/message-bubble.component.ts
@@ -1,17 +1,30 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input, signal } from '@angular/core';
 import { Message } from '../../entities/message';
-import { User } from '../../entities/user';
+import { User, userStore } from '../../entities/user';
 import { Profile } from '../../entities/profile';
+import { AvatarComponent } from '../../shared/ui/avatar/avatar.component';
 
 @Component({
   selector: 'app-message-bubble',
-  imports: [],
+  imports: [
+    AvatarComponent
+  ],
   templateUrl: './message-bubble.component.html',
   styleUrl: './message-bubble.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessageBubbleComponent {
+  private readonly userStore = inject(userStore)
+
   messageData = input.required<Message>() 
   senderData = input.required<User>()
   ProfileData = input.required<Profile>()
+
+  isYour = computed<boolean>(() => {
+    if(this.messageData().senderId === this.userStore.currentUser()?.id){
+      return true
+    }else{
+      return false
+    }
+  })
 }
