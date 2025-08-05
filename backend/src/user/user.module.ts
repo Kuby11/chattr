2a656515bbcbb8f9e36b8823ce14ce31e447c61d
Kuby11 <saab112468@gmail.com>
2025-08-05 23:10:48 +0400

    added RoomService to UserModule for enhanced user functionality and integration with chat features

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -3,13 +3,15 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ProfileService } from 'src/profile/profile.service';
+import { RoomService } from 'src/chat/room/room.service';
 
 @Module({
   controllers: [UserController],
   providers: [
     UserService,
     PrismaService,
-    ProfileService
+    ProfileService,
+    RoomService
   ],
   exports: [UserService,ProfileService],
 })
