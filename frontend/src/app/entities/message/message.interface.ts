d698ea3b5ac107bc411c5bc63705eee2e4d4a6c0
Kuby11 <saab112468@gmail.com>
2025-08-05 23:11:07 +0400

    added message.service export and extended Message interface with chatId and senderId; introduced MessageData interface for enhanced message handling

--- frontend/src/app/entities/message/message.interface.ts
+++ frontend/src/app/entities/message/message.interface.ts
@@ -1,6 +1,19 @@
+import { Profile } from "../profile";
+import { User } from "../user";
+
 export interface Message {
 	id: string;
 	content: string;
+	chatId: string;
+	senderId: string;
 	createdAt: Date;
 	updatedAt: Date;
+}
+
+export interface MessageData {
+	message: Message
+	senderData: {
+		senderInfo: User
+		senderProfile: Profile
+	}
 }
