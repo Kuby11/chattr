1632ce1e5afad24476b447d6a1d1f32d397c6c0a
Kuby11 <saab112468@gmail.com>
2025-08-05 23:10:11 +0400

    refactored chat page component to improve structure and user experience; updated template to display chat rooms with user avatars and optimized scrolling behavior for better message visibility

--- frontend/src/app/pages/chat-page/chat-page.component.ts
+++ frontend/src/app/pages/chat-page/chat-page.component.ts
@@ -1,25 +1,24 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
-import { ChatWidgetComponent } from '../../widgets/chat-widget/chat-widget.component';
-import { firstValueFrom } from 'rxjs';
-import { HlmAvatarImageDirective, HlmAvatarComponent, HlmAvatarFallbackDirective } from '@spartan-ng/ui-avatar-helm';
-import { RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, computed, effect, ElementRef, inject, OnInit, signal, viewChild } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HlmSkeletonComponent } from '@spartan-ng/ui-skeleton-helm';
-import { FirstLetterPipe } from '../../shared/pipes/first-letter.pipe';
 import { ShortenUsernamePipe } from '../../shared/pipes/shorten-username.pipe';
 import { currentPageService } from '../../shared/services';
+import { Member, RoomService, roomStore } from '../../entities/room';
+import { AvatarComponent } from '../../shared/ui/avatar/avatar.component';
+import { MessageInputComponent } from '../../widgets/message-input/message-input.component';
+import { userStore } from '../../entities/user';
+import { Profile } from '../../entities/profile';
 import { friendsStore } from '../../entities/friend/friend.store';
 
 
 @Component({
   selector: 'app-chat-page',
   imports: [
-    ChatWidgetComponent,
-    HlmAvatarComponent,
-    HlmAvatarFallbackDirective,
-    HlmAvatarImageDirective,
     HlmSkeletonComponent,
     ShortenUsernamePipe,
-    FirstLetterPipe,
+    RouterOutlet,
+    AvatarComponent,
+    MessageInputComponent,
     RouterLink,
   ],
   templateUrl: './chat-page.component.html',
@@ -28,13 +27,41 @@ import { friendsStore } from '../../entities/friend/friend.store';
 })
 export class ChatPageComponent implements OnInit {
   private readonly pageService = inject(currentPageService);
-  
-  friendStore = inject(friendsStore)
+  private readonly userStore = inject(userStore)
+  private readonly inputEl = viewChild<ElementRef<HTMLInputElement>>('el')
+  private readonly chatEl = viewChild<ElementRef<HTMLElement>>('chatEl')
+ 
+  protected skeletonItems = signal<number[]>(Array(20).fill(0))
 
-  skeletonItems = signal<number[]>(Array(20).fill(0))
+  currentUser = computed(() => this.userStore.currentUser()!)
+  roomStore = inject(roomStore)
+  friendStore = inject(friendsStore)
+  
+  get chatOffset(){
+    if(!this.inputEl){
+      return 0
+    }else{
+      return (this.inputEl() as any).chatOffset()
+    }
+  }
+ 
+  scrollToBottom = effect(() => {
+    const el = this.chatEl()
+    setTimeout(() => {
+      if(el){
+        el.nativeElement.scrollIntoView({ behavior: 'auto', block: 'end' })
+        el.nativeElement.scrollTop = el.nativeElement.offsetHeight
+      }
+    }, 500);
+  })
+  
+  getUserProfile(members: Member[]): Profile {
+    return members.find(member => member.userId !== this.currentUser().id)?.user.profile as Profile
+  }
 
   ngOnInit() {
-    this.friendStore.loadMyFriends()
+    this.scrollToBottom
+    this.roomStore.loadRooms()
     this.pageService.setPage('chat');
   }
 }
